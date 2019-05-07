8aff52a65fdfd11680b41a33b5cfda1deb3d6733
Edwin Miranda <edwin.miranda@akc.co.local>
2019-05-07 10:18:22 -0500

    commit ayudas

---
+++ INSERT_TER_TERMINAL.sql
@@ -0,0 +1,42 @@
+--ACTUALIZACION E INSERCION DE NUEVOS MOVILES EN TER_TERMINAL
+insert into AKCWWW.TER_TERMINAL 
+values (683,353779103724995,'cel_J6Samsung_carlosdavila',
+        TO_DATE('2019-01-24', 'yyyy-mm-dd'),'edwin.miranda',
+        'ACTIVO',NULL,NULL,NULL,NULL);
+        insert into AKCWWW.TER_TERMINAL 
+values (684,353779103727048,'cel_J6Samsung_renypedrozo',
+        TO_DATE('2019-01-24', 'yyyy-mm-dd'),'edwin.miranda',
+        'ACTIVO',NULL,NULL,NULL,NULL);
+        insert into AKCWWW.TER_TERMINAL 
+values (685,353779103734150,'cel_J6Samsung_jhonatanalarcon',
+        TO_DATE('2019-01-24', 'yyyy-mm-dd'),'edwin.miranda',
+        'ACTIVO',NULL,NULL,NULL,NULL);
+        insert into AKCWWW.TER_TERMINAL 
+values (686,353779103693539,'cel_J6Samsung_wilmerbayona',
+        TO_DATE('2019-01-24', 'yyyy-mm-dd'),'edwin.miranda',
+        'ACTIVO',NULL,NULL,NULL,NULL);
+        insert into AKCWWW.TER_TERMINAL 
+values (687,353779103694990,'cel_J6Samsung_luiscastellanos',
+        TO_DATE('2019-01-24', 'yyyy-mm-dd'),'edwin.miranda',
+        'ACTIVO',NULL,NULL,NULL,NULL);
+        insert into AKCWWW.TER_TERMINAL 
+values (688,353779103692317,'cel_J6Samsung_willianmora',
+        TO_DATE('2019-01-24', 'yyyy-mm-dd'),'edwin.miranda',
+        'ACTIVO',NULL,NULL,NULL,NULL);
+        insert into AKCWWW.TER_TERMINAL 
+values (689,353779103719342,'cel_J6Samsung_arbeypalomeque',
+        TO_DATE('2019-01-24', 'yyyy-mm-dd'),'edwin.miranda',
+        'ACTIVO',NULL,NULL,NULL,NULL);
+        insert into AKCWWW.TER_TERMINAL 
+values (690,353779103722908,'cel_J6Samsung_juanpulido',
+        TO_DATE('2019-01-24', 'yyyy-mm-dd'),'edwin.miranda',
+        'ACTIVO',NULL,NULL,NULL,NULL);
+                insert into AKCWWW.TER_TERMINAL 
+values (691,353779103696706,'cel_J6Samsung_januarymartinez',
+        TO_DATE('2019-01-24', 'yyyy-mm-dd'),'edwin.miranda',
+        'ACTIVO',NULL,NULL,NULL,NULL);
+                insert into AKCWWW.TER_TERMINAL 
+values (692,353779103770113,'cel_J6Samsung_ismaelfernandez',
+        TO_DATE('2019-01-24', 'yyyy-mm-dd'),'edwin.miranda',
+        'ACTIVO',NULL,NULL,NULL,NULL);
+        
---
+++ radicados_novedades2019.sql
@@ -0,0 +1,8 @@
+-->novedades-2019
+--nuevos
+201900000215 --> edwin miranda
+201900000216 --> luis suarez
+
+--antiguos
+201800005681--edwin
+201800005678--luis
